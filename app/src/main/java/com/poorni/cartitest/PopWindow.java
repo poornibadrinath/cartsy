c348af9fe9846f28438daad307126781abcb8f76
eliezerjff <eliezerjff.77@gmail.com>
2023-01-17 21:58:47 +0100

    Pop Window 1

---
+++ app/src/main/java/com/poorni/cartitest/PopWindow.java
@@ -0,0 +1,14 @@
+package com.poorni.cartitest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_window);
+    }
+}
